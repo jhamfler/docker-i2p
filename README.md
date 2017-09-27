0bcdf20186c495c90a4a0372be850089110572d7
Johannes Hamfler <jh+github@z7k.de>
2017-09-27 04:56:54 +0200

    fixed firewall issue

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # docker-i2p
 Docker Image for the I2P-router
+
+Run './build.sh' and then './run.sh'. Then you can connect to your i2prouter on your server with 'ssh -L 4444:127.0.0.1:4444 -L 7657:127.0.0.1:7657 example-domain.tld -l username-on-your-server'.
--- build.sh
+++ build.sh
@@ -1,2 +1,58 @@
 #!/bin/bash
+# check if curl is available
+curl=$(command -v curl)
+if [[ "$curl" == "" ]]
+then
+  echo curl is not available
+  echo cannot determine external ip
+  exit
+fi
+
+# check if shuf is available
+shuf=$(command -v shuf)
+if [[ "$shuf" == "" ]]
+then
+  echo shuf is not available
+  echo cannot generate random port number
+  exit
+fi
+
+# check if sed is available
+sed=$(command -v sed)
+if [[ "$sed" == "" ]]
+then
+  echo sed is not available
+  echo cannot generate random port number
+  exit
+fi
+
+# check if Dockerfile can be found
+if [[ ! -e Dockerfile ]]
+then
+  echo Dockerfile is not present
+  echo maybe call the script from the same directory where the Dockerfile is
+  exit
+fi
+
+# copy the templates and edit them afterwards
+cp run.sh-template run.sh
+cp Dockerfile-template Dockerfile
+
+# change ip in dockerfile
+ip=$(curl -s https://4.ifcfg.me/)
+line="ENV EXTERNAL_IP "
+sed -i "s/${line}.*/${line}${ip}/g" Dockerfile
+
+# change port in dockerfile and run.sh
+port=$(shuf -i 9000-31000 -n 1)
+line="ENV I2P_PORT "
+sed -i "s/${line}.*/${line}${port}/g" Dockerfile
+
+line="EXPOSE randomportnumber"
+sed -i "s/${line}.*/EXPOSE ${port}/g" Dockerfile
+
+line="i2pport=randomport"
+sed -i "s/${line}.*/i2pport=${port}/g" run.sh
+
+# build the image
 docker build -t i2p .
