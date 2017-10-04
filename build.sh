c20f5baa7c4842bd841a84dcd9c505e2e0179491
Johannes Hamfler <johannes.hamfler@stud.htwk-leipzig.de>
2017-10-04 22:35:17 +0200

    support for mutiple container

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-# copy the templates and edit them afterwards
-cp run.sh-template run.sh
-cp Dockerfile-template Dockerfile
-
+die=0
 # check if curl is available
 curl=$(command -v curl)
 if [[ "$curl" == "" ]]
 then
   echo curl is not available
-  echo cannot determine external ip
-  exit
+  echo Cannot determine external IP.
+  die=1
 fi
 
 # check if shuf is available
@@ -17,8 +14,8 @@ shuf=$(command -v shuf)
 if [[ "$shuf" == "" ]]
 then
   echo shuf is not available
-  echo cannot generate random port number
-  exit
+  echo Cannot generate random port number.
+  die=1
 fi
 
 # check if sed is available
@@ -26,33 +23,50 @@ sed=$(command -v sed)
 if [[ "$sed" == "" ]]
 then
   echo sed is not available
-  echo cannot change random port number
+  echo Cannot change random port number.
+  die=1
+fi
+
+# check if the Dockerfile can be found
+if [[ ! -e Dockerfile-template ]]
+then
+  echo Dockerfile-template is not present
+  echo Maybe call the script from the same directory where the Dockerfile-template is.
+  die=1
+fi
+
+if [[ $die == 1 ]]
+then
   exit
 fi
 
-# check if Dockerfile can be found
-if [[ ! -e Dockerfile ]]
-then
-  echo Dockerfile is not present
-  echo maybe call the script from the same directory where the Dockerfile is
-  exit
-fi
+# Generate a random portnumber for the i2p-router.
+# This step is normally done automatically by the i2p-router with this
+# port range and recommended by the documentation.
+# It needs to be done this way as docker needs to be started with portforward
+# and the number would be generated after the start inside the container.
+port=$(shuf -i 9000-31000 -n 1)
+
+# copy the templates and edit them afterwards
+dockerfile="Dockerfile-"$port
+runfile="run-"$port".sh"
+cp run.sh-template $runfile
+cp Dockerfile-template $dockerfile
 
 # change ip in dockerfile
 ip=$(curl -s https://4.ifcfg.me/)
 line="ENV EXTERNAL_IP "
-sed -i "s/${line}.*/${line}${ip}/g" Dockerfile
+sed -i "s/${line}.*/${line}${ip}/g" $dockerfile
 
 # change port in dockerfile and run.sh
-port=$(shuf -i 9000-31000 -n 1)
 line="ENV I2P_PORT "
-sed -i "s/${line}.*/${line}${port}/g" Dockerfile
+sed -i "s/${line}.*/${line}${port}/g" $dockerfile
 
 line="EXPOSE randomportnumber"
-sed -i "s/${line}.*/EXPOSE ${port}/g" Dockerfile
+sed -i "s/${line}.*/EXPOSE ${port}/g" $dockerfile
 
 line="i2pport=randomport"
-sed -i "s/${line}.*/i2pport=${port}/g" run.sh
+sed -i "s/${line}.*/i2pport=${port}/g" $runfile
 
 # build the image
-docker build -t i2p .
+docker build -f $dockerfile -t i2p-$port
